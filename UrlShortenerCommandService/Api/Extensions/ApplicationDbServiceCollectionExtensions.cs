ad3b1258c7e5ca3f59beb8985304b9ce3577679d
josedanielcr <jdcanales1905@gmail.com>
2025-09-02 22:36:06 -0600

    Feat: basic structure of the clean architecture approach with layers and basic interactions

---
+++ UrlShortenerCommandService/Api/Extensions/ApplicationDbServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Persistance.Database;
+
+namespace Api.Extensions;
+
+public static class ApplicationDbServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationDb(this IServiceCollection services, IConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configuration);
+        services.AddDbContext<ApplicationDbContext>();
+        return services;
+    }
+}
---
+++ UrlShortenerCommandService/Persistance/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Persistance.Database;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    public DbSet<ShortUrl> ShortUrls { get; set; }
+}
