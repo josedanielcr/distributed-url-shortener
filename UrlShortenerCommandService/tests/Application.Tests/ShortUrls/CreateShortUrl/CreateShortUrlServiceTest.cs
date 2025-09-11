4839ae788bfa8f9e29500481b076094e86c1d6de
josedanielcr <jdcanales1905@gmail.com>
2025-09-10 23:06:25 -0600

    Feat: Tests architecture for Application, Api and Persistence projects. Application's layer first test

---
+++ UrlShortenerCommandService/tests/Application.Tests/ShortUrls/CreateShortUrl/CreateShortUrlServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Application.ShortUrls.CreateShortUrl;
+using Domain.Entities;
+using Domain.Repository;
+using Moq;
+
+namespace Application.Tests.ShortUrls.CreateShortUrl;
+
+public class CreateShortUrlServiceTest
+{
+    [Fact]
+    public async Task GivenValidUrl_WhenCreateShortUrl_ThenPersistsAndReturnsId()
+    {
+        //Arrange
+        var shortUrl = new ShortUrl
+        {
+            Id = 1,
+            OriginalUrl = "https://example.com/some/long/path?utm_source=test",
+            ShortenedUrl = "abc123",
+            HostMachine = "server-01",
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+        var mockShortUrlRepository = new Mock<IShortUrlRepository>();
+        mockShortUrlRepository.Setup(repo => repo.AddShortUrlAsync(shortUrl))
+            .ReturnsAsync(1);
+        var createShortUrlService = new CreateShortUrlService(mockShortUrlRepository.Object);
+        
+        //Act
+        var result = await createShortUrlService.CreateShortUrl(shortUrl);
+        
+        //Asserts
+        result.Value.Equals(1);
+    }
+}
